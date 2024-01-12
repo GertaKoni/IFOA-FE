4eb7309fc59338c7efd11780e231e4784778d54c
GertaKoni <148806771+GertaKoni@users.noreply.github.com>
2024-01-12 14:00:20 +0100

    React Netflix

---
+++ Progetto React_Netflix/reactnetflix/src/App.jsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+
+
+function App() {
+
+  return (
+    <>
+      <CustomHeader/>
+      <CustomMain />
+      <CustomFooter/> 
+    </>
+  )
+}
+
+export default App
