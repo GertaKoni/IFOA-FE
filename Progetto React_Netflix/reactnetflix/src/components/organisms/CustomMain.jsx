fe32e692953bfa0484718282bc0f4d6ac8343222
GertaKoni <148806771+GertaKoni@users.noreply.github.com>
2024-01-12 16:07:54 +0100

    React Netflix

--- Progetto React_Netflix/reactnetflix/src/components/organisms/CustomMain.jsx
+++ Progetto React_Netflix/reactnetflix/src/components/organisms/CustomMain.jsx
@@ -6,6 +6,23 @@ const CustomMain = () => {
         <>
         <CustomPageTitleSection/>
         
+
+        <div className="section1 p-5">
+        <h4>Batman</h4>
+        <CustomGallery url="http://www.omdbapi.com/?apikey=b8ac787a&s=batman"/>
+        </div>
+
+        
+        <div className="section1 p-5">
+        <h4>Matrix</h4>
+        <CustomGallery url="http://www.omdbapi.com/?apikey=b8ac787a&s=matrix"/>
+        </div>
+        
+        <div className="section1 p-5">
+        <h4>Attack on Titan</h4>
+        <CustomGallery url="http://www.omdbapi.com/?apikey=b8ac787a&s=attack%20on%20titan"/>
+        </div>
+
         <div className="section1 p-5">
         <h4>Star Wars</h4>
         <CustomGallery url="http://www.omdbapi.com/?apikey=b8ac787a&s=star%20wars" />
@@ -14,11 +31,6 @@ const CustomMain = () => {
         <div className="section1 p-5">
         <h4>Doctor Who</h4>
         <CustomGallery url="http://www.omdbapi.com/?apikey=b8ac787a&s=doctor%20who"/>
-        </div>
-
-        <div className="section1 p-5">
-        <h4>Lord of the Rings</h4>
-        <CustomGallery url="http://www.omdbapi.com/?apikey=b8ac787a&s=lord%20of%20the%20rings"/>
         </div>
         </>
     )
