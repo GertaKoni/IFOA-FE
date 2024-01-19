67587bd96ad5f4e89547b224c265c0893f6244fd
GertaKoni <148806771+GertaKoni@users.noreply.github.com>
2024-01-19 17:41:14 +0100

    weather meteo App

--- Progett Meteo-Weather/meteoweather/src/App.jsx
+++ Progett Meteo-Weather/meteoweather/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import CustomNavbar from './components/CustomNavbar';
+import CustomFooter from './components/CustomFooter';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import WeatherDetails from './components/WeatherDetails'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App mb-5">
+      <BrowserRouter>
+        <CustomNavbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/details/:id" element={<WeatherDetails />} />
+        </Routes>
+        <CustomFooter />
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
